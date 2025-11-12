0d349db5889aef8437f806bd223cb8aa47ee5b6e
mmle04 <149206740+mmle04@users.noreply.github.com>
2025-11-12 00:22:34 -0500

    Comments
    Comments made to clear up future plans

--- job_parser.py
+++ job_parser.py
@@ -2,3 +2,8 @@ import os
 
 def parse_job(path):
     print("parsing job")
+
+    #parse file, add values to like an array or something
+
+    # next person will just compare how similar the two arrays
+    # are for now, we will make it more complex later
--- resume_parser.py
+++ resume_parser.py
@@ -2,3 +2,8 @@ import os
 
 def parse_resume(path):
     print("parsing resume")
+
+    #parse file, add values to like an array or something
+
+    # next person will just compare how similar the two arrays
+    # are for now, we will make it more complex later
