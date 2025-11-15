dedf4f3b19e0dcc5252c0712dc1eaf7e9c114a25
rgronau22 <rgronau22@vt.edu>
2025-11-14 22:20:35 -0500

    changes to job_parser

--- job_parser.py
+++ job_parser.py
@@ -1,9 +1,14 @@
 import os
+import re
+from docx import Document  # pip install python-docx
+
+# first obtain keywords from job description
 
 def parse_job(path):
     print("parsing job")
 
-    #parse file, add values to like an array or something
+    # parse file, add values to like an array or something
 
     # next person will just compare how similar the two arrays
     # are for now, we will make it more complex later
+
