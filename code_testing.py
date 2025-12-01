d9b53ae374c9e032699ecc793011df9609803454
mmle04 <149206740+mmle04@users.noreply.github.com>
2025-12-01 13:14:12 -0500

    Print statements in code_testing
    Print statements in code_testing

--- code_testing.py
+++ code_testing.py
@@ -6,12 +6,14 @@ class Tests(unittest.TestCase):
 
     # ---- TEST 1: INVALID FILE TYPE ----
     def testInvalidType1_resume(self):
+        print("testing testInvalidType1_resume...")
         invalid_path = "fakefile.txt"
         with self.assertRaises(FileNotFoundError):
             parse_resume(invalid_path)
 
 
     def testInvalidType1_job(self):
+        print("testing testInvalidType1_job...")
         invalid_path = "fakefile.mp4"
         with self.assertRaises(FileNotFoundError):
             parse_job(invalid_path)
@@ -19,12 +21,14 @@ class Tests(unittest.TestCase):
 
     # ---- TEST 2: NONEXISTENT FILE ----
     def testNonexistentFile_resume(self):
+        print("testing testNonexistentFile_resume...")
         invalid_path = "this_file_does_not_exist.docx"
 
         with self.assertRaises(FileNotFoundError):
             parse_resume(invalid_path)
 
     def testNonexistentFile_job(self):
+        print("testing testNonexistentFile_job...")
         invalid_path = "this_file_does_not_exist.txt"
 
         with self.assertRaises(FileNotFoundError):
